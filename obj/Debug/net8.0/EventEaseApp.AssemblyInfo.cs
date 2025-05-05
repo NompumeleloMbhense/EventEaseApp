5e18f65df13d1852b39640786ab0c69c6434711c
NompumeleloMbhense <nsmbhense6@gmail.com>
2025-05-05 20:32:54 +0200

    Add minimalLayout with sunset theme styling

--- obj/Debug/net8.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventEaseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d46c366aab488ca17a5cb91568a1824cd2a77f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
